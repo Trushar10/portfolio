7beaf7178a643eb36a4f918c34b979980263819a
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2021-05-26 23:18:42 +0530

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,71 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import Hero from "../components/Hero"
+import Services from "../components/Services"
+import Jobs from "../components/Jobs"
+import Projects from "../components/Projects"
+import Blogs from "../components/Blogs"
+
+export default ({ data }) => {
+  const {
+    allStrapiProjects: { nodes: projects },
+    allStrapiBlogs: { nodes: blogs },
+  } = data
+
+  return (
+    <Layout>
+      <Hero />
+      <Services />
+      <Jobs />
+      <Projects projects={projects} title="featured projects" showLink />
+      <Blogs blogs={blogs} title="latest articles" showLink />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        github
+        id
+        description
+        title
+        url
+        image {
+          localFile {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+    allStrapiBlogs(sort: { fields: strapiId, order: DESC }, limit: 3) {
+      nodes {
+        date(formatString: "MMMM Do, YYYY")
+        slug
+        content
+        description
+        category
+        title
+        image {
+          localFile {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
